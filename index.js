f0e23aee021ee762b05cbdbec0eff5b74569469e
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-25 13:35:33 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,57 @@
+var GitHubApi = require("github");
+var _ = require('lodash');
+
+var pickResultData = [
+    'id',
+    'owner.login',
+    'full_name',
+    'description',
+    'html_url',
+    'homepage',
+    'default_branch',
+    'created_at'
+];
+
 module.exports = {
+    /**
+     * Pick API result.
+     *
+     * @param input
+     * @returns {{}}
+     */
+    pickResultData: function (input) {
+        var result = {};
+
+        pickResultData.forEach(function (dataKey) {
+            if (!_.isUndefined(_.get(input, dataKey, undefined))) {
+
+                _.set(result, dataKey, _.get(input, dataKey));
+            }
+        });
+
+        return result;
+    },
+
+    /**
+     * Authenticate gitHub user.
+     *
+     * @param dexter
+     * @param github
+     */
+    gitHubAuthenticate: function (dexter, github) {
+
+        if (dexter.environment('GitHubUserName') && dexter.environment('GitHubPassword')) {
+
+            github.authenticate({
+                type: dexter.environment('GitHubType') || "basic",
+                username: dexter.environment('GitHubUserName'),
+                password: dexter.environment('GitHubPassword')
+            });
+        } else {
+            this.fail('A GitHubUserName and GitHubPassword environment variable is required for this module');
+        }
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +59,23 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        var github = new GitHubApi({
+            // required 
+            version: "3.0.0"
+        });
+
+        this.gitHubAuthenticate(dexter, github);
+
+        if (!step.input('name').first()) {
+
+            this.fail('[name] input variable need for this module');
+        } else {
+
+            github.repos.create(step.inputs(), function (err, repoInfo) {
+
+                err ? this.fail(err) : this.complete(this.pickResultData(repoInfo));
+            }.bind(this));
+        }
     }
 };
